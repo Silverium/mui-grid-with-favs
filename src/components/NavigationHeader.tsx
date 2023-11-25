30fcbc03edd33be878edbcb056c1783e5c35c525
Silverium <soldeplatadeveloper@gmail.com>
2023-11-25 12:20:52 +0100

    Add GitHub link to NavigationHeader

--- src/components/NavigationHeader.tsx
+++ src/components/NavigationHeader.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import { AppBar, Toolbar, Typography, Button, IconButton, Box } from '@mui/material';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 const NavigationHeader: React.FC = () => {
   return (
     <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h6">Carbon Certificates App</Typography>
-        <Button component={Link} to="/" color="inherit">Home</Button>
-        <Button component={Link} to="/certificates" color="inherit">Certificates</Button>
-        <Button component={Link} to="/favorites" color="inherit">Favorites</Button>
+      <Toolbar style={{ justifyContent: 'space-between' }}>
+        <Box display="flex" flexDirection={"row"}>
+          <Typography variant="h6">Carbon Certificates App</Typography>
+          <Button component={Link} to="/" color="inherit">Home</Button>
+          <Button component={Link} to="/certificates" color="inherit">Certificates</Button>
+          <Button component={Link} to="/favorites" color="inherit">Favorites</Button>
+        </Box>
+        <IconButton component={Link} to="https://github.com/Silverium/mui-grid-with-favs" color="inherit">
+          <GitHubIcon />
+        </IconButton>
       </Toolbar>
     </AppBar>
   );
