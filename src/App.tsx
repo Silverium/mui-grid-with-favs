0d551f9898543c98eec6cfda1efe65feeeacb137
Silverium <soldeplatadeveloper@gmail.com>
2023-11-27 16:28:04 +0100

    Update dependencies and add README content to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import * as React from "react";
 import "./styles.css";
-import { BrowserRouter, Route, Routes, Link as RouterLink } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import TableFilter from "./components/certificatesGrid/TableFilter";
 import FavoriteCertificates from "./components/certificatesGrid/FavoriteCertificates";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import {  Box, Link, ListItemText, Typography } from "@mui/material";
+import Markdown from 'react-markdown'
+import {  Box, Container } from "@mui/material";
 import Toaster from "./components/Toaster";
 import NavigationHeader from "./components/NavigationHeader";
 import { Provider } from 'react-redux';
@@ -14,7 +13,13 @@ import store from './store/globalStore';
 const queryClient = new QueryClient();
 
 export default function App() {
-
+  const [readmeContent, setReadmeContent] = React.useState<string>("");
+  React.useEffect(() => {
+    fetch("https://raw.githubusercontent.com/Silverium/mui-grid-with-favs/master/README.md")
+      .then(res => res.text())
+      .then(setReadmeContent)
+      .catch(console.error);
+  }, [])
 
   return (
     <BrowserRouter>
@@ -25,65 +30,11 @@ export default function App() {
 
             <Routes>
               <Route path="/" Component={() => {
-                return <>
-                  <Typography variant="h3" component="h1">
-                    Tech stack used
-                  </Typography>
-
-                  <List>
-                    <ListItem alignItems="center">
-                      <ListItemText sx={{ textAlign: "center" }}>
-                        <Link href="https://mui.com/components/data-grid/">
-                          MUI Data Grid
-                        </Link>
-
-                      </ListItemText>
-                    </ListItem >
-                    <ListItem alignItems="center">
-                      <ListItemText sx={{ textAlign: "center" }}>
-                        <Link href="https://react-query.tanstack.com/">
-                          React Query
-                        </Link>
-                      </ListItemText>
-                    </ListItem>
-                  </List>
-                  <Typography variant="h3" component="h1">
-                    Features required
-                  </Typography>
-                  <List>
-                    <ListItem>
-                      <ListItemText sx={{ fontSize: "20px", color: "GrayText" }}>
-                        <Link component={RouterLink} to="/certificates">"Certificates"</Link> page - the list of available Carbon certificates
-                      </ListItemText>
-                      <List>
-                        <ListItem >
-                          <ListItemText >
-                            Copying the certificate ID to the clipboard
-                          </ListItemText>
-                        </ListItem >
-                        <ListItem >
-                          <ListItemText >
-                            Saving the certificate as a favourite (client-side only)
-                          </ListItemText>
-                        </ListItem >
-                      </List>
-                    </ListItem >
-                    <ListItem>
-                      <ListItemText sx={{ fontSize: "20px", color: "GrayText" }}>
-                        <Link component={RouterLink} to="/favorites">"Favorites"</Link> page - the list of saved Carbon Certificates
-                      </ListItemText>
-                      <List>
-                        <ListItem >
-                          <ListItemText >
-                            Same page as "Certificates" with a possibility to remove the certificate from favourites
-                          </ListItemText>
-                        </ListItem >
-                      </List>
-                    </ListItem >
-
-
-                  </List>
-                </>
+                return(
+                  <Container maxWidth="md">
+                    <Markdown>{readmeContent}</Markdown>
+                  </Container>
+                )
               }} />
               <Route path="/certificates" Component={TableFilter} />
               <Route path="/favorites" Component={FavoriteCertificates} />
